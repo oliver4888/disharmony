c6949ee92584c97db9e791d8e2453ca8324f2b19
benji7425 <benji7425@outlook.com>
2019-04-01 13:31:05 +0100

    Add client functionality

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,78 @@
-export default class Client
+import { Client as DjsClient, Message as DjsMessage, Channel as DChannel } from "discord.js"
+import { SimpleEventDispatcher, SignalDispatcher, ISimpleEvent } from "strongly-typed-events"
+import Command from "./commands/command"
+import logger from "./utilities/logger";
+import { initialize as initializeDb } from "./database/db-client";
+import Message from "./models/discord/message";
+
+export interface IClient
 {
-    
+    readonly name: string
+    readonly botID: string
+    readonly commands: Command[]
+    readonly channels: Map<string, DChannel>
+    readonly onMessage: ISimpleEvent<Message>
+}
+
+export default class Client implements IClient
+{
+    private client: DjsClient
+
+    public readonly onBeforeLogin = new SignalDispatcher()
+    public readonly onReady = new SignalDispatcher()
+    public readonly onMessage = new SimpleEventDispatcher<Message>()
+
+    get botID() { return /[0-9]{18}/.exec(this.client.user.toString())![0] }
+    get channels(): Map<string, DChannel> { return this.client.channels }
+
+    public async initialize(token: string)
+    {
+        this.client.on("message", dMsg => this.handleMessage(dMsg))
+        this.client.on("debug", this.onDebug)
+        this.client.on("guildCreate", guild => logger.consoleLog(`Added to guild ${guild.name}`))
+
+        //remove newlines from token, sometimes text editors put newlines at the start/end but this causes problems for discord.js' login
+        await this.client.login(token.replace(/\r?\n|\r/g, ""))
+
+        logger.consoleLog(`Registered bot ${this.client.user.username}`)
+    }
+
+    private async handleMessage(djsMessage: DjsMessage)
+    {
+        //ignore messages from self
+        if (djsMessage.member.id === djsMessage.member.guild.me.id)
+            return
+
+        djsMessage.reply("Ping")
+    }
+
+    private onDebug(msg: string)
+    {
+        msg = msg.replace(/Authenticated using token [^ ]+/, "Authenticated using token [redacted]")
+        if (!/[Hh]eartbeat/.exec(msg)) //ignore regular heartbeat messages that would bloat the log file
+            logger.debugLog(msg)
+    }
+
+    constructor(public name: string, public commands: Command[] = new Array<Command>(), dbConnectionString: string = "nedb://nedb-data")
+    {
+        this.client = new DjsClient({
+            messageCacheMaxSize: 16,
+            disabledEvents: [
+                "CHANNEL_PINS_UPDATE",
+                "GUILD_BAN_ADD",
+                "GUILD_BAN_REMOVE",
+                "PRESENCE_UPDATE",
+                "TYPING_START",
+                "USER_NOTE_UPDATE",
+                "USER_SETTINGS_UPDATE"
+            ]
+        })
+
+        initializeDb(dbConnectionString)
+
+        Error.stackTraceLimit = Infinity
+        process.on("uncaughtException", err => logger.debugLog(`Unhandled exception!\n${err.message}\n${err.stack}`, true))
+        process.on("exit", () => logger.debugLog("Shutdown"))
+        process.on("SIGINT", () => process.exit())
+    }
 }
