38e47f0b9b9fc30c692ec7d7d952d4a1545460c9
Benji Higgins <benji7425@pm.me>
2019-04-01 23:32:22 +0100

    Add exports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello world!")
+import Client, { IClient } from "./client"
+import BotGuildMember from "./models/discord/guild-member";
+import BotGuild from "./models/discord/guild";
+import Message from "./models/discord/message";
+
+export { Client, BotGuildMember, BotGuild, Message, IClient }
