2b49278950f6db850994f68a7a1fafa6cff890f5
Benji Higgins <benji7425@pm.me>
2019-05-08 00:25:01 +0100

    Replace loadCredentials with loadConfig

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import BotGuildMember from "./models/discord/guild-member";
 import BotMessage from "./models/discord/message";
 import SubDocument from "./models/sub-document"
 import getWorker from "./utilities/fork-worker"
-import loadCredentials from "./utilities/load-credentials"
+import loadConfig from "./utilities/load-configuration"
 import Logger from "./utilities/logger"
 import NotifyPropertyChanged from "./utilities/notify-property-changed"
 
@@ -23,7 +23,7 @@ export
     PermissionLevel,
     Logger,
     getWorker as forkWorkerClient,
-    loadCredentials,
+    loadConfig,
     NotifyPropertyChanged,
     SubDocument,
 }
---
+++ src/utilities/load-configuration.ts
@@ -0,0 +1,27 @@
+import { existsSync } from "fs"
+import { resolve } from "path"
+import Config from "../models/internal/config";
+
+export default function (configPath: string = "./config.json"): { config: Config, isLocalDb: boolean }
+{
+    let config
+    if (existsSync(configPath))
+        config = require(resolve(process.cwd(), configPath))
+    else
+        throw new Error("No config file found!")
+
+    if (!isConfigValid(config))
+        throw new Error("Invalid config!")
+
+    return {
+        config,
+        isLocalDb: config.dbConnectionString.startsWith("nedb://"),
+    }
+}
+
+function isConfigValid(config: Config)
+{
+    // Ensure the user hasn't typed non-strings into these json fields
+    return config.dbConnectionString && typeof config.dbConnectionString === "string"
+        && config.token && typeof config.token === "string"
+}
