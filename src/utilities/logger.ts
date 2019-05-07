76cd907541ef122f617cc44d6f95e2cd2d10b15e
Benji Higgins <benji7425@pm.me>
2019-05-08 00:25:49 +0100

    Update logger
    Logs to file/console should make a bit more sense. Also add promise signature for log methods in case you want to wait for a log write to finish for some reason.

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,38 +1,33 @@
-// tslint:disable: no-console
 import * as SimpleFileWriter from "simple-file-writer"
 
 const consoleLogWriter = new SimpleFileWriter(process.cwd() + "/console.log")
 const debugLogWriter = new SimpleFileWriter(process.cwd() + "/debug.log")
 
-function doLog(message: string, debug: boolean, error?: Error | boolean)
+async function doLog(message: string, debugOnly: boolean, error?: Error | boolean)
 {
-    const prefix = error ? "[ERROR]" : debug ? "[DEBUG]" : "[INFO]"
-    let logStr = [`[${process.pid}] [${new Date().toUTCString()}]`, prefix, message].join(" ")
+    const prefix = error ? "[ERROR]" : debugOnly ? "[DEBUG]" : "[INFO]"
+    const messageStr = [`[${process.pid}] [${new Date().toUTCString()}]`, prefix, message].join(" ")
+    let consoleStr = messageStr, debugStr = messageStr
 
-    if (debug)
-        if (error instanceof Error)
-        {
-            let errorString = ""
-            if (error.message)
-                errorString += `\n${error.message}`
-            if (error.stack)
-                errorString += `\n${error.stack}`
-            console.error(logStr + errorString)
-        }
-        else
-            console.log(logStr)
+    if (error instanceof Error)
+    {
+        consoleStr += ` | ${error.message}`
+        debugStr += `\n\t${error.message}\n\t${error.stack}`
+    }
 
-    logStr += "\n"
+    await debugLogWriter.write(debugStr + "\n")
 
-    if (!debug)
-        consoleLogWriter.write(logStr)
-
-    debugLogWriter.write(logStr)
+    if (!debugOnly)
+    {
+        // tslint:disable-next-line: no-console
+        console.log(consoleStr)
+        await consoleLogWriter.write(consoleStr + "\n")
+    }
 }
 
 export default {
-    consoleLog: (message: string) => doLog(message, false),
-    debugLog: (message: string) => doLog(message, true),
-    consoleLogError: (message: string, error?: Error) => doLog(message, false, error || true),
-    debugLogError: (message: string, error?: Error) => doLog(message, true, error || true),
+    consoleLog: (message: string): void | Promise<void> => doLog(message, false).catch(),
+    debugLog: (message: string): void | Promise<void> => doLog(message, true).catch(),
+    consoleLogError: (message: string, error?: Error): void | Promise<void> => doLog(message, false, error || true).catch(),
+    debugLogError: (message: string, error?: Error): void | Promise<void> => doLog(message, true, error || true).catch(),
 }
