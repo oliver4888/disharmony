701cb2cbca0feae1544893b92b103b4c600e3430
benji7425 <benji7425@pm.me>
2019-05-23 14:21:32 +0100

    Add FriendlyError abstract class with DocumentError derived
    FriendlyError represents an Error instance with a friendly message attached

---
+++ src/core/friendly-error.ts
@@ -0,0 +1,19 @@
+export abstract class FriendlyError extends Error
+{
+    public friendlyMessage: string
+    constructor(
+        message: string,
+        friendlyMessage: string | boolean = false,
+    )
+    {
+        super(message)
+
+        if (friendlyMessage === true)
+            this.friendlyMessage = message
+        else if (typeof friendlyMessage === "string")
+            this.friendlyMessage = friendlyMessage
+
+        // See https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+        Object.setPrototypeOf(this, FriendlyError.prototype)
+    }
+}
