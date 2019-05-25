bd3100671fd7a734898ddfaeea43bd389979a947
benji7425 <benji7425@pm.me>
2019-05-25 13:53:51 +0100

    Fix spelling mistake

--- src/commands/command-error.ts
+++ src/commands/command-error.ts
@@ -9,7 +9,7 @@ export class CommandError extends FriendlyError
             case CommandErrorReason.UserMissingPermissions:
                 return "You do not have permission to use this command"
             case CommandErrorReason.IncorrectSyntax:
-                return "Incorrect syntax! See correct syntac with the `help` command"
+                return "Incorrect syntax! See correct syntax with the `help` command"
             case CommandErrorReason.BotMissingGuildPermissions:
                 return "The bot has not been granted the necessary permissions in this server. Please grant the permissions and try again. Details can be found in the readme you used to invite the bot."
         }
