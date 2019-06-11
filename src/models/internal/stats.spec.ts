a1498f5516ae981f9bdda4177719c017f69b60a9
Benji Higgins <benji7425@pm.me>
2019-06-11 22:26:19 +0100

    Fix stats command returning wrongly divised numbers
    Closes #14

---
+++ src/models/internal/stats.spec.ts
@@ -0,0 +1,61 @@
+import { Expect, Setup, Test, TestFixture } from "alsatian";
+import { Client as DjsClient } from "discord.js"
+import { IMock, Mock } from "typemoq";
+import Stats from "./stats";
+
+@TestFixture("Stats model transformations")
+export class StatsTestFixture
+{
+    private djsClient: IMock<DjsClient>
+
+    @Setup
+    public setup()
+    {
+        this.djsClient = Mock.ofType<DjsClient>();
+    }
+
+    @Test()
+    public zero_hour_zero_minute_uptime_string_correct()
+    {
+        // ARRANGE
+        this.djsClient
+            .setup(x => x.uptime)
+            .returns(() => 15 * 1000)
+
+        // ACT
+        const sut = new Stats(this.djsClient.object)
+
+        // ASSERT
+        Expect(sut.uptimeStr).toBe("00:00:15")
+    }
+
+    @Test()
+    public zero_hour_multi_minute_uptime_string_correct()
+    {
+        // ARRANGE
+        this.djsClient
+            .setup(x => x.uptime)
+            .returns(() => 1815 * 1000)
+
+        // ACT
+        const sut = new Stats(this.djsClient.object)
+
+        // ASSERT
+        Expect(sut.uptimeStr).toBe("00:30:15")
+    }
+
+    @Test()
+    public multi_hour_multi_minute_uptime_string_correct()
+    {
+        // ARRANGE
+        this.djsClient
+            .setup(x => x.uptime)
+            .returns(() => 5415 * 1000)
+
+        // ACT
+        const sut = new Stats(this.djsClient.object)
+
+        // ASSERT
+        Expect(sut.uptimeStr).toBe("01:30:15")
+    }
+}
--- src/models/internal/stats.ts
+++ src/models/internal/stats.ts
@@ -11,14 +11,14 @@ export default class Stats
     {
         const secsTruncated = Math.trunc(ms / 1000)
         const hrs = Math.floor(secsTruncated / 3600)
-        const mins = Math.floor(secsTruncated - (hrs * 3600) / 60)
+        const mins = Math.floor((secsTruncated - (hrs * 3600)) / 60)
         const secs = secsTruncated - (hrs * 3600) - (mins * 60)
 
         let hoursStr = hrs.toString(), minsStr = mins.toString(), secsStr = secs.toString()
 
-        if (hrs < 10) { hoursStr = "0" + hrs }
-        if (mins < 10) { minsStr = "0" + mins }
-        if (secs < 10) { secsStr = "0" + secs }
+        if (hrs < 10) hoursStr = "0" + hrs
+        if (mins < 10) minsStr = "0" + mins
+        if (secs < 10) secsStr = "0" + secs
         return `${hoursStr}:${minsStr}:${secsStr}`
     }
 
