55dd241467757a224d6b7ad63e8f8000483aeaff
benji7425 <benji7425@pm.me>
2019-10-08 13:43:14 +0100

    Add WorkerAction class
    Base class which can be inherited from to provide some boilerplate bootstrapping code for a module being run in a forked worker process

--- src/utilities/export-generator.ts
+++ src/utilities/export-generator.ts
@@ -1,9 +1,11 @@
-import { LightClient, loadConfig, Logger } from ".."
+import { Logger } from ".."
 import PendingExports, { PendingExport } from "../models/internal/pending-exports"
+import WorkerAction from "./worker-action"
 
-export default class ExportGenerator
+export default class ExportGenerator extends WorkerAction
 {
-    public async processExportsForAllPendingEntries()
+    /** @override */
+    public async invoke()
     {
         await Logger.debugLog("Beginning iteration of pending exports")
 
@@ -26,34 +28,6 @@ export default class ExportGenerator
 
         // Handle what to do if the file is too big for Discord's upload limit
     }
-
-    constructor(
-        private client: LightClient,
-    ) { }
 }
 
-/* TODO This is almost copy-pasted from Activity Monitor's InactivityManager module.
-   Perhaps it would be a good idea to create a unified concept of a "forked module" or something,
-   basically a base class that can bootstrap itself like this, call the main invoke method, then exit.
-   It would also do the kind of rudimentary error handling seen below. */
-if (!module.parent)
-{
-    const configPath = process.argv[2]
-    const { config } = loadConfig(undefined, configPath)
-    const client = new LightClient(config)
-    const exportGenerator = new ExportGenerator(client)
-    client.login(config.token)
-        .then(async () =>
-        {
-            await exportGenerator.processExportsForAllPendingEntries()
-            await client.destroy()
-            await Logger.debugLog("Finished processing pending exports, exiting worker")
-            process.exit(0)
-        })
-        .catch(async err =>
-        {
-            await Logger.debugLogError("Error running the export generator", err)
-            await Logger.logEvent("ErrorStartingPendingExportGenerator")
-            process.exit(1)
-        })
-}
+WorkerAction.bootstrapModuleIfInWorker(ExportGenerator)
---
+++ src/utilities/worker-action.ts
@@ -0,0 +1,58 @@
+import { resolve } from "path"
+import { forkWorkerClient, LightClient, loadConfig, Logger } from ".."
+
+/** Base class representing a module which can be easily launched in a worker module.
+ *  Will automatically connect to Discord and provide a LightClient instance
+ */
+export default abstract class WorkerAction
+{
+    public static bootstrapModuleIfInWorker<T extends WorkerAction>(moduleCtor: new (client: LightClient) => T)
+    {
+        if (!module.parent)
+        {
+            const configPath = process.argv[2]
+            const { config } = loadConfig(undefined, configPath)
+            const client = new LightClient(config)
+            const module = new moduleCtor(client)
+
+            client.login(config.token)
+                .then(async () =>
+                {
+                    await module.invoke()
+                    await client.destroy()
+                    await Logger.debugLog("Exiting worker")
+                    process.exit(0)
+                })
+                .catch(async err =>
+                {
+                    await Logger.debugLogError("Error invoking module in worker process", err)
+                    await Logger.logEvent("ErrorInWorkerModule")
+                    process.exit(1)
+                })
+        }
+    }
+
+    /** @abstract */
+    public async invoke()
+    {
+        return
+    }
+
+    constructor(
+        protected client: LightClient,
+    ) { }
+}
+
+export async function invokeWorkerAction(path: string, useMainProcess: boolean, mainProcessClient: LightClient): Promise<void>
+export async function invokeWorkerAction(path: string, useMainProcess: boolean, configPath: string): Promise<void>
+export async function invokeWorkerAction(path: string, useMainProcess: boolean, processArg: LightClient | string)
+{
+    await Logger.debugLog(`Loading worker module in ${useMainProcess ? "main" : "worker"} process`)
+    if (useMainProcess)
+    {
+        const moduleCtor = (await import(path)).default
+        await new moduleCtor(processArg).invoke()
+    }
+    else
+        forkWorkerClient(resolve(path), processArg as string)
+}
